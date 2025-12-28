bdc5b2e4b916544e23e0984b05a5493154f8e8d9
FanaticalBera <khanbsw4597@gmail.com>
2025-12-28 23:44:54 +0900

    fix: Pass environment variables to Keystatic handler

--- functions/api/keystatic/[[path]].ts
+++ functions/api/keystatic/[[path]].ts
@@ -1,10 +1,13 @@
 import { makeGenericAPIRouteHandler } from '@keystatic/core/api/generic'
 import keystaticConfig from '../../../keystatic.config'
 
-const handler = makeGenericAPIRouteHandler({
-    config: keystaticConfig,
-})
-
 export const onRequest = async (context: any) => {
+    const handler = makeGenericAPIRouteHandler({
+        config: keystaticConfig,
+        clientId: context.env.KEYSTATIC_GITHUB_CLIENT_ID,
+        clientSecret: context.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+        secret: context.env.KEYSTATIC_SECRET,
+    })
+
     return handler(context.request)
 }
